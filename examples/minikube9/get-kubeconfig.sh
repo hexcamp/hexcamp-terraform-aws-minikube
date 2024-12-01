b85766244b4e927937d1f5713f3ec55d4f2aadbe
Jim Pick <jim@jimpick.com>
2024-12-01 15:07:54 -0800

    minikube9

---
+++ examples/minikube9/get-kubeconfig.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+CLUSTER=$(pwd | sed 's,^.*\/,,')
+
+scp ubuntu@$CLUSTER.localnet.farm:/home/ubuntu/kubeconfig kubeconfig_dns
+perl -pi -e "s/kubernetes/$CLUSTER/g" kubeconfig_dns
+perl -pi -e 's/admin@//g' kubeconfig_dns
+perl -pi -e "s/admin/admin_$CLUSTER/g" kubeconfig_dns
+./flatten.sh > kubeconfig2
+kubectl --kubeconfig ./kubeconfig2 get node -o wide
+cp -v ~/.kube/config ~/.kube/config.$(date '+%Y%m%d-%H%M%S')
+cp -v ./kubeconfig2 ~/.kube/config
---
+++ examples/minikube9/main.tf
@@ -0,0 +1,30 @@
+module "minikube" {
+  #source = "scholzj/minikube/aws"
+  #source = "github.com/scholzj/terraform-aws-minikube"
+  #source = "github.com/praveen18k/terraform-aws-minikube-1?ref=patch-1"
+  #source = "github.com/praveen18k/terraform-aws-minikube?ref=184e32c8347b6fa437671436b63c37193bf7b46e"
+  #source = "git::https://github.com/praveen18k/terraform-aws-minikube.git?ref=184e32c8347b6fa437671436b63c37193bf7b46e"
+  source = "./../.."
+
+  aws_region          = "eu-central-1"
+  cluster_name        = "minikube9"
+  aws_instance_type   = "m7a.medium"
+  ssh_public_key      = "~/.ssh/id_rsa.pub"
+  aws_subnet_id       = "subnet-0dff924c15b061ac5"
+  hosted_zone         = "localnet.farm"
+  hosted_zone_private = false
+  ami_image_id        = "ami-0d2246efddc8414dc"
+
+  tags = {
+    Application = "Minikube"
+  }
+  
+  addons = [
+    # "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+    # "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/csi-driver.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/metrics-server.yaml",
+    # "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+    # "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml",
+  ]
+}
+
