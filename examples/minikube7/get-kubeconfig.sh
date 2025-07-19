199ada62bdf77cc446383f627b22627903273a04
Jim Pick <jim@jimpick.com>
2025-07-19 12:51:30 -0700

    Add minikube11

--- examples/minikube7/get-kubeconfig.sh
+++ examples/minikube7/get-kubeconfig.sh
@@ -10,3 +10,6 @@ perl -pi -e "s/admin/admin_$CLUSTER/g" kubeconfig_dns
 kubectl --kubeconfig ./kubeconfig2 get node -o wide
 cp -v ~/.kube/config ~/.kube/config.$(date '+%Y%m%d-%H%M%S')
 cp -v ./kubeconfig2 ~/.kube/config
+
+echo argocd cluster rm $CLUSTER -y
+echo argocd cluster add $CLUSTER -y
--- send-ips-as-cloudevent.sh
+++ send-ips-as-cloudevent.sh
@@ -5,17 +5,20 @@ set -euxo pipefail
 export AWS_ACCESS_KEY_ID=$LOCALNET_FARM_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$LOCALNET_FARM_SECRET_ACCESS_KEY
 
-MINIKUBE10=$(cd examples/minikube10; tofu output -raw public_ip)
-echo minikube10: $MINIKUBE10
 MINIKUBE7=$(cd examples/minikube7; tofu output -raw public_ip)
 echo minikube7: $MINIKUBE7
 MINIKUBE8=$(cd examples/minikube8; tofu output -raw public_ip)
 echo minikube8: $MINIKUBE8
 MINIKUBE9=$(cd examples/minikube9; tofu output -raw public_ip)
 echo minikube9: $MINIKUBE9
+MINIKUBE10=$(cd examples/minikube10; tofu output -raw public_ip)
+echo minikube10: $MINIKUBE10
+MINIKUBE11=$(cd examples/minikube11; tofu output -raw public_ip)
+echo minikube11: $MINIKUBE11
 
 jq -n " \
   .minikube10_ip=\"$MINIKUBE10\" | \
+  .minikube11_ip=\"$MINIKUBE11\" | \
   .minikube7_ip=\"$MINIKUBE7\" | \
   .minikube8_ip=\"$MINIKUBE8\" | \
   .minikube9_ip=\"$MINIKUBE9\"" > /tmp/ips.json
