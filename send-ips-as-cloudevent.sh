c504e0652ba44626a11210733b3c128dafa5980a
Jim Pick <jim@jimpick.com>
2025-07-27 18:29:56 -0700

    Remove minikube7 and minikube8

--- send-ips-as-cloudevent.sh
+++ send-ips-as-cloudevent.sh
@@ -5,10 +5,10 @@ set -euxo pipefail
 export AWS_ACCESS_KEY_ID=$LOCALNET_FARM_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$LOCALNET_FARM_SECRET_ACCESS_KEY
 
-MINIKUBE7=$(cd examples/minikube7; tofu output -raw public_ip)
-echo minikube7: $MINIKUBE7
-MINIKUBE8=$(cd examples/minikube8; tofu output -raw public_ip)
-echo minikube8: $MINIKUBE8
+#MINIKUBE7=$(cd examples/minikube7; tofu output -raw public_ip)
+#echo minikube7: $MINIKUBE7
+#MINIKUBE8=$(cd examples/minikube8; tofu output -raw public_ip)
+#echo minikube8: $MINIKUBE8
 MINIKUBE9=$(cd examples/minikube9; tofu output -raw public_ip)
 echo minikube9: $MINIKUBE9
 MINIKUBE10=$(cd examples/minikube10; tofu output -raw public_ip)
@@ -19,8 +19,6 @@ echo minikube11: $MINIKUBE11
 jq -n " \
   .minikube10_ip=\"$MINIKUBE10\" | \
   .minikube11_ip=\"$MINIKUBE11\" | \
-  .minikube7_ip=\"$MINIKUBE7\" | \
-  .minikube8_ip=\"$MINIKUBE8\" | \
   .minikube9_ip=\"$MINIKUBE9\"" > /tmp/ips.json
 
 # https://stackoverflow.com/questions/7642743/how-to-generate-random-numbers-in-the-busybox-shell
